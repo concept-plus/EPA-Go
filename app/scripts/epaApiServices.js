ca33ffd5f4a1925b6eba2523c6bc53f9f8e0a38a
Mike Mathis <mmathis@conceptplusllc.com>
2015-12-23 19:18:22 -0500

    update server urls

--- app/scripts/epaApiServices.js
+++ app/scripts/epaApiServices.js
@@ -9,9 +9,36 @@ services.factory('apiQueryService', ['$http', '$q', function($http, $q) {
     var BASE_URL = 'http://52.90.111.204:8181/relay';
 
     var factory = {
-        queryAQI: function(options) {
 
-            var url = BASE_URL + '/aqi?zipcode=' + options.zipcode;
+        queryAQICurrent: function(options) {
+
+            var url = BASE_URL + '/aqi-current?zipcode=' + options.zipcode;
+
+            console.log('querying: ' + url);
+
+            // write to api-well
+            // angular.element('#api-called').append('<p><a href="' + url + '" target="_blank">' + url + '</a></p>');
+
+            var deferred = $q.defer();
+            var httpPromise = $http.get(url);
+
+            httpPromise.success(function(response) {
+                // try {
+                    deferred.resolve(response);
+                // } catch(error) {
+                //     deferred.reject('invalid response');
+                // }
+            });
+            httpPromise.error(function(response, statusCode) {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
+        },
+
+        queryAQIRange: function(options) {
+
+            var url = BASE_URL + '/aqi-range?zipcode=' + options.zipcode;
 
             console.log('querying: ' + url);
 
